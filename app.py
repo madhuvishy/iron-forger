4cd7d9e6774de7988edd92956fb5e165da67e3fe
Rose Ames <rose@happyspork.com>
2014-04-22 16:58:35 -0400

    add stub routes

--- app.py
+++ app.py
@@ -5,5 +5,31 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world!"
 
+@app.route('/proposal')
+def vote():
+    return "List of proposals goes here"
+
+@app.route('/proposal/new')
+def new_proposal():
+    return "Submit a proposal!"
+
+@app.route('/proposal/<name>')
+def proposal_name(name):
+    return "proposal %s" % name
+
+@app.route('/user')
+def register():
+    return "Register for an awesome account!"
+
+@app.route('/user/<name>')
+def user_name(name):
+    return "user %s" % name
+
+@app.route('/project/<name>')
+def project_name(name):
+    return "project %s" % name
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
